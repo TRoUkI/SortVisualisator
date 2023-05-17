20d9b83a35ca65620f70c7e8c56a89661e66a548
TRoUkI <romanjdemchuk@gmail.com>
2023-05-17 15:58:17 +0300

    Modify original algorithms to InputArrayClass

---
+++ src/test/java/algorithms/AlgorithmMyTestTest.java
@@ -0,0 +1,13 @@
+package test.java.algorithms;
+
+import main.java.algorithms.AlgorithmMyTest;
+import org.junit.jupiter.api.Test;
+
+class AlgorithmMyTestTest {
+
+    @Test
+    void algorithmBody() {
+        new AlgorithmMyTest().AlgorithmBody();
+
+    }
+}
